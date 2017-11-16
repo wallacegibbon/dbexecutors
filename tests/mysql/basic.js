7bf83b2a4045160a5d7647ce240586f393e4f1cf
wallacegibbon <wallacegibbon@aliyun.com>
2017-11-16 14:36:04 +0800

    Use colourlogger@1.2.2 to support no-color log output

--- tests/mysql/basic.js
+++ tests/mysql/basic.js
@@ -20,6 +20,7 @@ const config = {
 
 const executor = getMysqlExecutor(config);
 //executor.disableLog();
+//executor.disableLogColor();
 
 
 
--- tests/redis/basic.js
+++ tests/redis/basic.js
@@ -7,6 +7,7 @@ const executor = getRedisExecutor({
 });
 
 //executor.disableLog();
+//executor.disableLogColor();
 
 
 
