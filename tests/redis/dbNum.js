3b8f135a559b3c94cc8be39ed1a5e36bd927c137
wallacegibbon <wallacegibbon@aliyun.com>
2017-11-22 17:27:55 +0800

    use newredis@1.3.0, now support dbNum in configuration

---
+++ tests/redis/dbNum.js
@@ -0,0 +1,21 @@
+const { getRedisExecutor } = require("../..");
+
+
+const executor = getRedisExecutor({
+  connectionLimit: 2,
+  password: "asdf",
+  dbNum: 3,
+});
+
+
+async function testCrud() {
+  console.log("Testing get empty key.".padEnd(75, "-"));
+  await executor.execute([ "set", "blahblah", "what?" ]);
+}
+
+(async function() {
+  await testCrud();
+
+})().catch(console.error);
+
+
