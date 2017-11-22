472ff08cd9b7d2af9adc74bb11e7377eb1c2fdb3
wallacegibbon <wallacegibbon@aliyun.com>
2017-11-22 13:48:51 +0800

    mysql test updated, and some comments updated

--- tests/mysql/basic.js
+++ tests/mysql/basic.js
@@ -25,24 +25,31 @@ const executor = getMysqlExecutor(config);
 
 
 async function testCrud() {
+  var r;
   console.log("Testing INSERT.".padEnd(75, "-"));
-  await executor.insert("t1", { name: "Wallace", age: 26, gender: 1 });
+  r = await executor.insert("t1", { name: "Wallace", age: 26, gender: 1 });
+  console.log("insert result:", r);
+
   await executor.insert("t1", { name: "wx", age: 26, gender: 1 });
   await executor.insert("t1", { name: "Luke", gender: 1 });
   await executor.insert("t1", { name: "Jessica", age: 28 });
 
   console.log("Testing DELETE.".padEnd(75, "-"));
-  await executor.remove("t1", { name: "wx" });
+  r = await executor.remove("t1", { name: "wx" });
+  console.log("delete result:", r);
 
   console.log("Testing UPDATE.".padEnd(75, "-"));
   await executor.update("t1", { age: 27 }, { name: "Wallace", gender: 1 });
-  await executor.update("t1", { gender: 2 }, 'name="Jessica" AND age=28');
+  r = await executor.update("t1", { gender: 2 }, 'name="Jessica" AND age=28');
+  console.log("update result:", r);
 
 
   console.log("Testing SELECT.".padEnd(75, "-"));
-  const r = await executor.select("t1", [ "name" ], { gender: 1 },
+  r = await executor.select("t1", [ "name" ], { gender: 1 },
                               "ORDER BY name DESC LIMIT 3 OFFSET 1");
-  console.log("r:", r);
+  console.log("select result:", r);
+  var { name } = r[0];
+  console.log("Use data unpack on result:", name);
 
   r.forEach(o => console.log(o.name));
 
