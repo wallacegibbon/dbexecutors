f2003db73fcd1f744b80a2230f647fa6d01c67bf
wallace <wallacegibbon@aliyun.com>
2018-03-30 20:33:16 +0800

    .

--- test/mongodb/basic.js
+++ test/mongodb/basic.js
@@ -3,7 +3,17 @@ const { getMongoExecutor } = require("../..")
 
 const executor = getMongoExecutor('mongodb://localhost:27017')
 
+
+function delay(ms) {
+    return new Promise((res, rej) => setTimeout(res, ms))
+}
+
+
 (async function() {
-    await executor.insert("testdb", "testcoll", { a: 3, b: 2})
+    await executor.connect()
+
+    const r = await executor.insert("testdb", "testcoll", { a: 3, b: 2})
+    console.log("r:", r)
 
 })().catch(console.error)
+
